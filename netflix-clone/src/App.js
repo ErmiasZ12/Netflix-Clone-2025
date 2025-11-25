d534e6d5d5b438a30a93d51994ca4caf7afbc6de
Ermiyas Werdofa <ermibkl@gmail.com>
2025-11-25 09:39:32 -0500

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
-
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <h1> Hello</h1>
+      <Header/>
+      <Footer />
     </div>
   );
 }
